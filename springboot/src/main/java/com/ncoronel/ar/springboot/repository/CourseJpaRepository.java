66157e7bd8d180ad8dc08fd1deb1ad624cdf402f
Coronel Nicolas <coronelnicolas77@hotmail.com>
2020-02-25 10:00:44 -0300

    se crea primer jpa repository

---
+++ springboot/src/main/java/com/ncoronel/ar/springboot/repository/CourseJpaRepository.java
@@ -0,0 +1,22 @@
+package com.ncoronel.ar.springboot.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ncoronel.ar.springboot.entity.Course;
+
+@Repository("courseJpaRepository")
+public interface CourseJpaRepository extends JpaRepository<Course, Serializable> {
+	
+	
+	//metodo que podemos crear para realizar query siguiendo la estructura de jpa repository
+	// findBy nombre de un campo en mayuscula, se puede usar and, or etc
+	
+	public abstract Course findByPrice(int price);
+	public abstract Course findByNameAndPrice(String name,int price);
+	public abstract List<Course> findByNameOrderByHours(String name);
+
+}
